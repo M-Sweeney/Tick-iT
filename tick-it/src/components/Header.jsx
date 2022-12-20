8cdc440b7302a7b83294908d40e3ec2126c3e8cf
Jordan Wolter <jwolter456@gmail.com>
2022-12-20 10:42:03 -0500

    made it so tick-it will bring you home

--- tick-it/src/components/Header.jsx
+++ tick-it/src/components/Header.jsx
@@ -1,8 +1,13 @@
-
+import { useNavigate } from 'react-router-dom'
 const Header = () => {
+  let navigate = useNavigate()
+
+  const navigateHome = () => {
+    navigate('')
+  }
   return (
         <div className="header">
-            <h2> Tick-It! </h2>
+            <h2 className="Tik-It"onClick={navigateHome}> Tick-It! </h2>
         </div>
 
   )
